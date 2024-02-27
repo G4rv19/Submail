9fe3f9037b923e661581489fcec635841af05a57
G4rv19 <garvsidana29@gmail.com>
2024-02-27 20:56:41 +1000

    fixed the repition of emails storing in db

--- main/views.py
+++ main/views.py
@@ -1,31 +1,36 @@
 from django.http import HttpResponse, HttpResponseBadRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import EmailForm
-from django.http import HttpResponseBadRequest #for the bad request error.
 from .models import Email
-from django.shortcuts import render
 import re
 
 def tasklist(request):
     return render(request, 'home.html')
 
 def email_view(request):
+    # Form submission handling
     if request.method == 'POST':
         form = EmailForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
             email_pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+
             # Check if the email matches the pattern
-            if re.match(email_pattern, email):  # Email is valid, print it to the terminal
-                # Proceed with your logic if needed
-                print(f"Entered email: {email}")
-                #save the email to the database
-                Email.objects.create(email=email)
-                return render(request, 'email.html', {'form': form})
+            if re.match(email_pattern, email):
+                # Check if the email already exists in the database
+                if Email.objects.filter(email=email).exists():
+                    print("Email already exists")
+                    return render(request, 'home.html')
+                else:
+                    print("Saved to db")
+                    # Save the email to the database
+                    Email.objects.create(email=email)
+                    return render(request, 'email.html', {'form': form})
             else:
                 print("Unexpected error: Invalid email format.")
                 return HttpResponseBadRequest("Unexpected error: Invalid email format.")
     else:
         form = EmailForm()
-    
+
+    # Render the form when the page is first loaded or on form validation failure
     return render(request, 'email.html', {'form': form})
